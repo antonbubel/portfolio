a495fc3e90e7b431135318e17f670967392e8067
Anton Bubel <a.bubel@itransition.com>
2024-02-04 17:48:16 +0100

    Configure Vercel web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  output: "server",
+  adapter: vercel({
+    webAnalytics: true,
+  }),
+  integrations: [tailwind(), svelte()],
 });
