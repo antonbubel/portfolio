839caa9b25fa015b07f3131f008202f397017d83
Anton Bubel <a.bubel@itransition.com>
2024-02-06 11:24:43 +0100

    Create a custom link and track the link_clicked event using Vercel Analytics

---
+++ src/components/Link.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from 'svelte';
+  import { track } from '@vercel/analytics';
+
+  export let href = '#';
+  export let target = '_self';
+  export let rel = '';
+  export let className = '';
+  export let ariaLabel = '';
+
+  let link;
+
+  onMount(() => {
+    link.addEventListener('click', () => {
+      // Send a custom event to Vercel Analytics
+      track('link_clicked', {
+        url: link.href
+      });
+    });
+  });
+</script>
+
+<a bind:this={link} {href} {target} {rel} {ariaLabel} class={className}>
+  <slot></slot>
+</a>
