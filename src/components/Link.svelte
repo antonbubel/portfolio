86ccc7900ac8dd556eadde34d4a9d644972dd07b
Anton Bubel <a.bubel@itransition.com>
2024-02-07 12:34:31 +0100

    Opt out of typescript, use jsdoc instead for Link component

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,26 +1,41 @@
-<script lang="ts">
-  import { onMount } from 'svelte';
-  import { track } from '@vercel/analytics';
+<script>
+  import { onMount } from "svelte";
+  import { track } from "@vercel/analytics";
 
-  export let href: string | undefined = '#';
-  
-  export let target: string | undefined = '_self';
-  export let rel: string | undefined = '';
-  export let className: string | undefined = '';
-  export let ariaLabel: string | undefined = '';
+  /** @type {HTMLAnchorElement} */
+  export let href = "#";
 
-  let link: HTMLAnchorElement;
+  /** @type {'_self' | '_blank' | '_parent' | '_top'} */
+  export let target = "_self";
+
+  /**  @type {string} */
+  export let rel = "";
+
+  /** @type {string} */
+  export let className = "";
+
+  /** @type {string} */
+  export let ariaLabel = "";
+
+  let link;
 
   onMount(() => {
-    link.addEventListener('click', () => {
+    link.addEventListener("click", () => {
       // Send a custom event to Vercel Analytics
-      track('link_clicked', {
-        url: link.href
+      track("link_clicked", {
+        url: link.href,
       });
     });
   });
 </script>
 
-<a bind:this={link} {href} {target} {rel} aria-label={ariaLabel} class={className}>
-  <slot></slot>
+<a
+  bind:this={link}
+  {href}
+  {target}
+  {rel}
+  aria-label={ariaLabel}
+  class={className}
+>
+  <slot />
 </a>
