4ef55e1763f841ac5114ef9cc0a478b8a831e59b
Anton Bubel <a.bubel@itransition.com>
2024-02-10 15:51:30 +0100

    Swap themes as they were set up incorrectly

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -1,4 +1,4 @@
-export const darkTheme: string = 'winter';
-export const lightTheme: string = 'night';
+export const darkTheme: string = 'night';
+export const lightTheme: string = 'winter';
 
 export const themes = [darkTheme, lightTheme] as const;
